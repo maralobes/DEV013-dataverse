9bda91ce1c8cd600c58eeaa27909104d69f3929f
Camila Ledezma <camilaledezmar@gmail.com>
2024-01-26 13:20:04 -0400

    render test3

--- src/view.js
+++ src/view.js
@@ -1,67 +1,46 @@
-import dataset from "./data/dataset";
+import dataset from "./data/dataset.js";
 
 export const renderItems = (data) => {
-  console.log(data)
+  console.log(dataset);
      
         const ulList = document.getElementById("placesList");
-
-        // const item = document.createElement("li");//dl
-        // const nameWork = document.createElement("h2");//dt
-        // const imageWork = document.createElement("img");//dt
-        // const shortDescrWork = document.createElement("p");//dt
-        // const descrWork = document.createElement("p");//dt
-        
-        // item.setAttribute("itemscope", "");
-        // item.setAttribute("itemtype", "name");
-        // item.setAttribute("class", "place" );
-        // nameWork.setAttribute("itemprop", "name");
-        // imageWork.setAttribute("itemprop", "image");
-        // shortDescrWork.setAttribute("itemprop", "shortDescription");
-        // descrWork.setAttribute("itemprop", "description");
-        
-
-        // nameWork.innerHTML = `${data.name}`;
-        // imageWork.innerHTML = data.imageUrl;
-        // shortDescrWork.innerHTML= data.shortDescription;
-        // descrWork.innerHTML = data.description;
-  
-        // for (let [dataName,] of Object.entries(data)) {
-          for (let i = 0; i<data.length; i ++){
-          
+          for (let i = 0; i< data.length; i ++){
+          console.log(data[i]);
           const item = document.createElement("li");
           
           const idWork = document.createElement("dl");//dl
           idWork.setAttribute("itemscope", "");
-          idWork.setAttribute("itemtype", "name");
-          idWork.innerHTML = data.id
+          idWork.setAttribute("itemtype", "id");
+          idWork.innerHTML = data[i].id;
 
           const nameWork = document.createElement("dt");//dt
           nameWork.setAttribute("itemprop", "name");
-          nameWork.innerHTML = data.name;
+          nameWork.innerHTML = data[i].name;
         
-          const imageWork = document.createElement("dt");//dt
-          imageWork.setAttribute("itemprop", "image");
-          imageWork.innerHTML = data.imageUrl;
+          const imageWork = document.createElement("img");
+          imageWork.setAttribute("src",data.imageUrl);
+          imageWork.setAttribute("alt",data.name);
+          imageWork.innerHTML = data[i].imageUrl;
 
           const shortDescrWork = document.createElement("dt");//dt
           shortDescrWork.setAttribute("itemprop", "shortDescription");
-          shortDescrWork.innerHTML= data.shortDescription;
+          shortDescrWork.innerHTML= data[i].shortDescription;
 
           const descrWork = document.createElement("dt");//dt
           descrWork.setAttribute("itemprop", "description");
-          descrWork.innerHTML = data.description;
+          descrWork.innerHTML = data[i].description;
          
-     
+          
           item.appendChild(idWork);
           item.appendChild(nameWork);
           item.appendChild(imageWork);
           item.appendChild(shortDescrWork);
           item.appendChild(descrWork);
-
+          ulList.appendChild(item);
         }
-        ulList.appendChild(item);
-      
-  
+
       console.log(data)
-  return dataset.forEach(renderItems) ;
+  return ulList;
 };
+
+renderItems(dataset);
