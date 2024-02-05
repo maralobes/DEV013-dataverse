22d558a8f4445966f37b1845bc09fe398754cd42
Camila Ledezma <camilaledezmar@gmail.com>
2024-02-05 16:38:24 -0400

    sortbyname function

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,46 +1,22 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
 export const example = () => {
-  return 'example';
+  return "example";
 };
 
 export const sortByName = (data, sortBy, sortOrder) => {
+  const sortWork = data.slice();
 
-    // const workOrder = data.toSorted((a,b) => {
-    //   if(sortOrder === 'asc'){    
-    //     if(a[sortBy] === b[sortBy]){
-    //       return 0;
-    //     }
-    //     if(a[sortBy]< b[sortBy]){
-    //       return -1;
-    //     }
-    //     return 1;
-    //   }
-    //   else {
-    //     if(a[sortBy] === b[sortBy]){
-    //       return 0;
-    //     }
-    //     if (a[sortBy] > b[sortBy]){
-    //       return -1;
-    //     }
-    //     return 1;
-    //   } 
-    // });
-    // return workOrder;
-      const sortWork = data.slice();
-    
-      sortWork.sort((a, b) => {
-        const nameA = a[sortBy].toLowerCase();
-        const nameB = b[sortBy].toLowerCase();
-    
-        if (sortOrder === 'asc') {
-          return nameA.localeCompare(nameB);
-        } else {
-          return nameB.localeCompare(nameA);
-        }
-      });
-    
-      return sortWork;
+  sortWork.sort((a, b) => {
+    const nameA = a[sortBy].toLowerCase();
+    const nameB = b[sortBy].toLowerCase();
 
+    if (sortOrder === "asc") {
+      return nameA.localeCompare(nameB);
+    } else {
+      return nameB.localeCompare(nameA);
+    }
+  });
+
+  return sortWork;
 };
-
--- src/main.js
+++ src/main.js
@@ -7,12 +7,9 @@ import data from "./data/dataset.js";
 const rootFill = document.getElementById("root");
 rootFill.appendChild(renderItems(data));
 
-// const buttons = document.getElementById("buttons");
-// buttons.appendChild(sortBy(data));
-  const selectSort = document.querySelector('select[data-testid="select-sort"]');
-  selectSort.addEventListener("change", function (e) {
-    const orderData = sortByName(data, "name", e.target.value);
-    rootFill.innerHTML = ""; // Utiliza rootFill en lugar de contenedor
-    rootFill.appendChild(renderItems(orderData));
-  });
-  
+const selectSort = document.querySelector('select[data-testid="select-sort"]');
+selectSort.addEventListener("change", function (event) {
+  const orderData = sortByName(data, "name", event.target.value);
+  rootFill.innerHTML = "";
+  rootFill.appendChild(renderItems(orderData));
+});
