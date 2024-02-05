47b9f652557632c586d113651e5e5e12077711ad
Maria <maria.villaloboschaves@gmail.com>
2024-02-05 17:17:07 -0600

    error nothing change

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { filteredData } from "./dataFunctions.js";
+import { sortByName } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
 import data from "./data/dataset.js";
 
 const rootFill = document.getElementById("root");
 rootFill.appendChild(renderItems(data));
+
+const selectSort = document.querySelector('select[data-testid="select-sort"]');
+selectSort.addEventListener("change", function (event) {
+  const orderData = sortByName(data, "name", event.target.value);
+  rootFill.innerHTML = "";
+  rootFill.appendChild(renderItems(orderData));
+});
 
 const filterBy = document.querySelector('select[data-testid="select-filter"]]');
 filterBy.appendChild()
