738039fdccac53728dc2561e6462107e0e0700cf
Maria <maria.villaloboschaves@gmail.com>
2024-02-05 17:11:24 -0600

    filterBy function v2

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,13 +1,38 @@
-/*export const dataFiltered = (data, filterBy, value) => {
-  const range = 0;
-  const filterByAnnualVisitors = data.filterBy(function(data){
-    if(range >= 0 && range <= 2000000){
-      return 
-  })
-  return 'dataFiltered';
-};*/
+export const filteredData = (data, filterBy, value) => { 
+  const filterWork = data.filter(function(value, data){
+    if (value === "firstRange"){
+      return data.facts[filterBy]<=2000000;
+    }
+    else if (value === "secondRange") {
+      return (data.facts[filterBy]>2000000 && data.facts[filterBy]<=4000000)
+    }
+    else if (value === "thirdRange") {
+      return (data.facts[filterBy]>4000000 && data.facts[filterBy]<=6000000)
+    }
+    else if (value === "fourthRange") {
+      return (data.facts[filterBy]>6000000 && data.facts[filterBy]<=8000000)
+    }
+    else {
+      return (data.facts[filterBy]>8000000 && data.facts[filterBy]<=10000000)
+    }
+  });
 
-export const filteredData = (data, filterBy, value) => {
-  const filterByAnnualVisitors= data.filterBy( value => value.facts.annualVisitors <="2000000" )
-  console.log(filterByAnnualVisitors);
-}
+  return filterWork
+};
+
+export const sortByName = (data, sortBy, sortOrder) => {
+  const sortWork = data.slice();
+
+  sortWork.sort((a, b) => {
+    const nameA = a[sortBy].toLowerCase();
+    const nameB = b[sortBy].toLowerCase();
+
+    if (sortOrder === "asc") {
+      return nameA.localeCompare(nameB);
+    } else {
+      return nameB.localeCompare(nameA);
+    }
+  });
+
+  return sortWork;
+};
