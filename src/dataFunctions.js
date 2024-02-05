09809ce30b8ccdcfb6571bfd875b3d190cc8717f
Maria <maria.villaloboschaves@gmail.com>
2024-02-05 14:59:27 -0600

    FilterBy function v1

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,13 @@
-// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
+/*export const dataFiltered = (data, filterBy, value) => {
+  const range = 0;
+  const filterByAnnualVisitors = data.filterBy(function(data){
+    if(range >= 0 && range <= 2000000){
+      return 
+  })
+  return 'dataFiltered';
+};*/
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return [];
-};
+export const filteredData = (data, filterBy, value) => {
+  const filterByAnnualVisitors= data.filter(data => data.facts.annualVisitors >= "0" && data.facts.annualVisitors <="2000000")
+  console.log(filterByAnnualVisitors);
+}
