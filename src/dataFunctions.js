bab98b803ef11958785cb203e2e240fb3aaa1f90
Camila Ledezma <camilaledezmar@gmail.com>
2024-02-05 12:58:52 -0400

    second try sort by

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -6,26 +6,41 @@ export const example = () => {
 
 export const sortByName = (data, sortBy, sortOrder) => {
 
-    const workOrder = data.toSorted((a,b) => {
-      if(sortOrder === 'asc'){    
-        if(a[sortBy] === b[sortBy]){
-          return 0;
+    // const workOrder = data.toSorted((a,b) => {
+    //   if(sortOrder === 'asc'){    
+    //     if(a[sortBy] === b[sortBy]){
+    //       return 0;
+    //     }
+    //     if(a[sortBy]< b[sortBy]){
+    //       return -1;
+    //     }
+    //     return 1;
+    //   }
+    //   else {
+    //     if(a[sortBy] === b[sortBy]){
+    //       return 0;
+    //     }
+    //     if (a[sortBy] > b[sortBy]){
+    //       return -1;
+    //     }
+    //     return 1;
+    //   } 
+    // });
+    // return workOrder;
+      const sortWork = data.slice();
+    
+      sortWork.sort((a, b) => {
+        const nameA = a[sortBy].toLowerCase();
+        const nameB = b[sortBy].toLowerCase();
+    
+        if (sortOrder === 'asc') {
+          return nameA.localeCompare(nameB);
+        } else {
+          return nameB.localeCompare(nameA);
         }
-        if(a[sortBy]< b[sortBy]){
-          return -1;
-        }
-        return 1;
-      }
-      else {
-        if(a[sortBy] === b[sortBy]){
-          return 0;
-        }
-        if (a[sortBy] > b[sortBy]){
-          return -1;
-        }
-        return 1;
-      } 
-    });
-    return workOrder;
+      });
+    
+      return sortWork;
+
 };
 
