0e6fe3804428b563e248005e74f8c9b6d85c87b8
Maria <maria.villaloboschaves@gmail.com>
2024-02-05 15:28:27 -0600

    Test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { example } from "./dataFunctions.js";
+import { filteredData } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
-
 import data from "./data/dataset.js";
 
 const rootFill = document.getElementById("root");
 rootFill.appendChild(renderItems(data));
+
+const filterBy = document.querySelector('select[data-testid="select-filter"]]');
+filterBy.appendChild()
