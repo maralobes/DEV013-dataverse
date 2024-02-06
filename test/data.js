f97d560999a1ca8aa405ecde5d0d41ef68c93112
Maria <maria.villaloboschaves@gmail.com>
2024-02-06 13:29:44 -0600

    data.js ready

--- test/data.js
+++ test/data.js
@@ -1,52 +1,77 @@
-// Aqui puedes agregar los datos que necesites para tus pruebas
-// al menos debe tener uno que se llame "data"
-
-export const data = [{
-  "id": "ada-lovelace",
-  "name": "Ada Lovelace",
-  "shortDescription": "Pionera en la programación, primera programadora de la historia.",
-  "description": "Una visionaria matemática británica del siglo XIX, es aclamada como la primera programadora de la historia. Su colaboración con Charles Babbage en la creación de la Máquina Analítica la llevó a desarrollar algoritmos, incluyendo el famoso \"algoritmo de Bernoulli\", destinados a ser procesados por una máquina. Su visión pionera la convirtió en una figura fundamental en la informática y la inteligencia artificial. Lovelace demostró que las computadoras podían ir más allá de simples cálculos matemáticos, anticipando un mundo donde la programación sería esencial en la resolución de problemas complejos, dejando una huella imborrable en la ciencia y la tecnología.",
-  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/ada-lovelace.jpg",
-  "facts": {
-    "yearOfBirth": "1815",
-    "yearOfDeath": "1852",
-    "birthPlace": "Londres, Reino Unido",
-    "mainField": "Ciencia de la Computación"
+export const data = [
+  {
+    id: "colosseum",
+    name: "The Colosseum",
+    shortDescription: "Iconic Roman amphitheater",
+    description: "The Colosseum, located in Rome, Italy, is a grand amphitheater built in AD 80. It is a symbol of ancient Roman engineering and once hosted gladiator contests. The elliptical structure could hold 50,000 spectators who gathered to witness various events, including battles between gladiators, animal hunts, and executions. The Colosseum's construction used concrete and sand, with an outer layer of travertine limestone. It stands as a testament to the architectural prowess of the ancient Romans and continues to be a major tourist attraction, attracting millions of visitors annually.",
+    imageUrl: "https://raw.githubusercontent.com/C-LedezmaRodriguez/DEV013-dataverse/main/src/images/1.%20The%20Colosseum.jpeg",
+    facts: {
+      yearOfBuilt: "80",
+      style: "Roman",
+      location: "Rome, Italy",
+      constructor: "Emperor Vespasian",
+      annualVisitors: 7000000,
+      isWonderOfTheWorld: false
+    }
   },
-  "extraInfo": {
-    "imageSource": "https://www.bing.com/images/create/genera-una-imagen-de-ada-lovelace2c-la-primera-prog/650a1eff8ff04b88833d5cdef0309441"
-  }
-},
-{
-  "id": "emmy-noether",
-  "name": "Emmy Noether",
-  "shortDescription": "Matemática destacada, teorema fundamental en álgebra abstracta.",
-  "description": "Una brillante matemática alemana, revolucionó la física teórica con sus contribuciones al teorema de conservación de la energía y el teorema de Noether, fundamentales en la teoría de la relatividad y la mecánica cuántica. A pesar de los obstáculos que enfrentó como mujer en la academia, sus investigaciones en álgebra abstracta y teoría de grupos son fundamentales en la física moderna. Su capacidad para unificar conceptos matemáticos y físicos marcó un hito en la historia de la ciencia, allanando el camino para futuras generaciones de científicas.",
-  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/emmy-noether.jpg",
-  "facts": {
-    "yearOfBirth": "1882",
-    "yearOfDeath": "1935",
-    "birthPlace": "Erlangen, Alemania",
-    "mainField": "Matemáticas"
+  {
+    id: "greatwallchina",
+    name: "Great Wall of China",
+    shortDescription: "World's longest wall with a rich history",
+    description: "The Great Wall of China, built over centuries, stretches thousands of miles. It served as a military defense and is a UNESCO World Heritage Site. The wall was constructed by various dynasties, including the Ming Dynasty. It consists of walls, watchtowers, and natural barriers. The Great Wall is an extraordinary example of ancient military architecture and engineering, adapting to the challenging terrain. It has become a symbol of China's rich history and culture, attracting around 10 million visitors annually. In 2007, it was officially recognized as one of the New Seven Wonders of the World.",
+    imageUrl: "https://raw.githubusercontent.com/C-LedezmaRodriguez/DEV013-dataverse/main/src/images/2.%20Great%20Wall%20of%20China.jpeg",
+    facts: {
+      yearOfBuilt: "7th century BC",
+      style: "Various",
+      location: "China",
+      constructor: "Various dynasties",
+      annualVisitors: 10000000,
+      isWonderOfTheWorld: true
+    }
   },
-  "extraInfo": {
-    "imageSource": "https://www.bing.com/images/create/create-an-image-of-of-a-german-woman2c-a-pioneering/6510ddb1e8254b928b8f8f1d5e95fea4"
-  }
-},
-{
-  "id": "hypatia-de-alejandria",
-  "name": "Hypatia de Alejandría",
-  "shortDescription": "Matemática y filósofa en la antigua Alejandría.",
-  "description": "Filósofa y matemática en la antigua Alejandría, desafió las normas de género de su época al destacar en una sociedad dominada por hombres. Su legado incluye importantes contribuciones a la geometría y la astronomía, así como su destacado papel como maestra y defensora del conocimiento en una época de agitación política. Hypatia demostró que las mujeres podían sobresalir en campos intelectuales y su valentía al mantener sus creencias filosóficas la convierte en un símbolo de resistencia y sabiduría en la historia de la ciencia.",
-  "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/hypatia-de-alejandria.jpg",
-  "facts": {
-    "yearOfBirth": "circa 360",
-    "yearOfDeath": "415",
-    "birthPlace": "Alejandría, Egipto",
-    "mainField": "Matemáticas, Filosofía"
+  {
+    id: "tajmahal",
+    name: "Taj Mahal",
+    shortDescription: "A marble mausoleum in India",
+    description: "The Taj Mahal, located in Agra, India, is an iconic marble mausoleum that stands as a testament to eternal love. Commissioned by Emperor Jahan in memory of his beloved wife Mumtaz Mahal, it was completed in 1653. This masterpiece of Mughal architecture combines intricate geometric patterns with exquisite craftsmanship. The Taj Mahal attracts approximately 8 million visitors annually, drawn by its stunning white marble facade, perfectly manicured gardens, and the poignant story of its creation. Recognized as a UNESCO World Heritage Site, the Taj Mahal remains an enduring symbol of India's rich cultural heritage and the enduring power of love.",
+    imageUrl: "https://raw.githubusercontent.com/C-LedezmaRodriguez/DEV013-dataverse/main/src/images/3.%20Taj%20Mahal.jpeg",
+    facts: {
+      yearOfBuilt: "1653",
+      style: "Mughal",
+      location: "Agra, India",
+      constructor: "Emperor Shah Jahan",
+      annualVisitors: 8000000,
+      isWonderOfTheWorld: true
+    }
   },
-  "extraInfo": {
-    "imageSource": "https://www.bing.com/images/create/create-a-vivid-digital-artwork-or-painting-that-de/6510df0ebe1a4807af22175452761e1a"
-  }
-},];
-
+  {
+    id: "eiffeltower",
+    name: "Eiffel Tower",
+    shortDescription: "Parisian landmark and iron masterpiece",
+    description: "Erected in 1889, the Eiffel Tower is an iconic iron lattice structure standing proudly in the heart of Paris, France. This architectural marvel symbolizes French ingenuity and innovation. Soaring to a height of 324 meters, the Eiffel Tower offers breathtaking panoramic views of Paris, attracting millions of visitors annually. Its intricate lattice design and Gothic Revival style make it a timeless symbol of the city. From its observation decks to its sparkling nighttime illumination, the Eiffel Tower remains a beloved landmark, serving as a testament to both engineering excellence and the romantic allure of the City of Light.",
+    imageUrl: "https://raw.githubusercontent.com/C-LedezmaRodriguez/DEV013-dataverse/main/src/images/4.%20Eiffel%20Tower.jpeg",
+    facts: {
+      yearOfBuilt: "1889",
+      style: "Gothic Revival",
+      location: "Paris, France",
+      constructor: "Gustave Eiffel",
+      annualVisitors: 7000000,
+      isWonderOfTheWorld: false
+    }
+  },
+  {
+    id: "pyramidofgiza",
+    name: "Pyramids of Giza",
+    shortDescription: "Ancient Egyptian pyramids with rich history",
+    description: "Built around 2580–2560 BC, the Pyramids of Giza stand as awe-inspiring ancient Egyptian structures located on the Giza Plateau near Cairo, Egypt. Dominated by the Great Pyramid of Khufu, these architectural marvels served as monumental tombs for pharaohs. The Great Pyramid, with its precise construction and alignment, remains one of the Seven Wonders of the Ancient World. Drawing approximately 1.5 million visitors annually, the Pyramids of Giza continue to captivate with their grandeur and historical significance, showcasing the advanced engineering skills of the ancient Egyptians and serving as a testament to their cultural and spiritual practices.",
+    imageUrl: "https://raw.githubusercontent.com/C-LedezmaRodriguez/DEV013-dataverse/main/src/images/5.%20Pyramids%20of%20Giza.jpeg",
+    facts: {
+      yearOfBuilt: "2580",
+      style: "Ancient Egyptian",
+      location: "Giza, Egypt",
+      constructor: "Pharaoh Khufu",
+      annualVisitors: 1500000,
+      isWonderOfTheWorld: true
+    }
+  },
+];
